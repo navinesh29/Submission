47c0b109dfc5c20e3e4264a6886435b08c96e76b
navinesh29 <153447784+navinesh29@users.noreply.github.com>
2023-12-11 21:33:09 +0530

    Add files via upload

--- python_task_1.py
+++ python_task_1.py
@@ -1,162 +1,92 @@
-Task 1 
-Question 1:
-
 import pandas as pd
 
-def generate_car_matrix(dataset_path):
-    # Load the CSV file into a DataFrame
-    df = pd.read_csv(dataset_path)
 
-    # Pivot the DataFrame using id_1 and id_2 columns
-    result_df = df.pivot(index='id_1', columns='id_2', values='car')
+def generate_car_matrix(df)->pd.DataFrame:
+    """
+    Creates a DataFrame  for id combinations.
 
-    # Fill NaN values with 0 (for cells without corresponding car values)
-    result_df = result_df.fillna(0)
+    Args:
+        df (pandas.DataFrame)
 
-    # Set diagonal values to 0
-    result_df.values[[range(result_df.shape[0])]*2] = 0
+    Returns:
+        pandas.DataFrame: Matrix generated with 'car' values, 
+                          where 'id_1' and 'id_2' are used as indices and columns respectively.
+    """
+    # Write your logic here
 
-    return result_df
-
-# Replace 'dataset-1.csv' with the actual path to the dataset file
-result_dataframe = generate_car_matrix('dataset-1.csv')
-
-# Display the result DataFrame
-print(result_dataframe)
-
-Question 2:
-
-import pandas as pd
-
-def get_type_count(dataset_path):
-    # Load the CSV file into a DataFrame
-    df = pd.read_csv(dataset_path)
-
-    # Add a new categorical column 'car_type' based on 'car' values
-    conditions = [
-        (df['car'] <= 15),
-        (df['car'] > 15) & (df['car'] <= 25),
-        (df['car'] > 25)
-    ]
-
-    choices = ['low', 'medium', 'high']
-
-    df['car_type'] = np.select(conditions, choices, default='unknown')
-
-    # Calculate the count of occurrences for each 'car_type' category
-    type_count = df['car_type'].value_counts().to_dict()
-
-    # Sort the dictionary alphabetically based on keys
-    type_count = dict(sorted(type_count.items()))
-
-    return type_count
-
-# Replace 'dataset-1.csv' with the actual path to the dataset file
-result_dict = get_type_count('dataset-1.csv')
-
-# Display the result dictionary
-print(result_dict)
-
-Question 3:
-import pandas as pd
-
-def get_bus_indexes(dataset_path):
-    # Load the CSV file into a DataFrame
-    df = pd.read_csv(dataset_path)
-
-    # Calculate the mean value of the 'bus' column
-    bus_mean = df['bus'].mean()
-
-    # Identify indices where 'bus' values are greater than twice the mean
-    bus_indexes = df[df['bus'] > 2 * bus_mean].index.tolist()
-
-    # Sort the indices in ascending order
-    bus_indexes.sort()
-
-    return bus_indexes
-
-# Replace 'dataset-1.csv' with the actual path to the dataset file
-result_list = get_bus_indexes('dataset-1.csv')
-
-# Display the result list
-print(result_list)
-
-Question 4:
-
-import pandas as pd
-
-def filter_routes(dataset_path):
-    # Load the CSV file into a DataFrame
-    df = pd.read_csv(dataset_path)
-
-    # Calculate the average value of the 'truck' column for each route
-    route_avg_truck = df.groupby('route')['truck'].mean()
-
-    # Filter routes where the average of 'truck' values is greater than 7
-    selected_routes = route_avg_truck[route_avg_truck > 7].index.tolist()
-
-    # Sort the list of selected routes in ascending order
-    selected_routes.sort()
-
-    return selected_routes
-
-# Replace 'dataset-1.csv' with the actual path to the dataset file
-result_list = filter_routes('dataset-1.csv')
-
-# Display the result list
-print(result_list)
-
-Question 5:
-def multiply_matrix(input_dataframe):
-    # Make a copy to avoid modifying the original DataFrame
-    modified_df = input_dataframe.copy()
-
-    # Apply the specified logic to each value in the DataFrame
-    modified_df = modified_df.applymap(lambda x: x * 0.75 if x > 20 else x * 1.25)
-
-    # Round values to 1 decimal place
-    modified_df = modified_df.round(1)
-
-    return modified_df
-
-# Assuming result_dataframe is the DataFrame obtained from the generate_car_matrix function in Question 1
-# Replace it with the actual DataFrame variable you have
-result_dataframe = generate_car_matrix('dataset-1.csv')
-
-# Apply the modification logic and display the result
-modified_dataframe = multiply_matrix(result_dataframe)
-print(modified_dataframe)
-
-Question 6:
-import pandas as pd
-
-def check_timestamp_completeness(df):
-    # Convert the 'timestamp' column to datetime format
-    df['timestamp'] = pd.to_datetime(df['timestamp'])
-
-    # Extract day and time information
-    df['day'] = df['timestamp'].dt.day_name()
-    df['hour'] = df['timestamp'].dt.hour
-
-    # Define expected days and hours
-    expected_days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    expected_hours = list(range(24))
-
-    # Check completeness for each (id, id_2) pair
-    completeness_series = df.groupby(['id', 'id_2']).apply(lambda group:
-        all(day in group['day'].unique() for day in expected_days) and
-        all(hour in group['hour'].unique() for hour in expected_hours)
-    )
-
-    return completeness_series
-
-# Assuming df is your DataFrame from dataset-2.csv
-# Replace it with the actual DataFrame variable  
-df = pd.read_csv('dataset-2.csv')
-
-# Apply the function and display the result
-result_series = check_timestamp_completeness(df)
-print(result_series)
+    return df
 
 
+def get_type_count(df)->dict:
+    """
+    Categorizes 'car' values into types and returns a dictionary of counts.
 
+    Args:
+        df (pandas.DataFrame)
+
+    Returns:
+        dict: A dictionary with car types as keys and their counts as values.
+    """
+    # Write your logic here
+
+    return dict()
+
+
+def get_bus_indexes(df)->list:
+    """
+    Returns the indexes where the 'bus' values are greater than twice the mean.
+
+    Args:
+        df (pandas.DataFrame)
+
+    Returns:
+        list: List of indexes where 'bus' values exceed twice the mean.
+    """
+    # Write your logic here
+
+    return list()
+
+
+def filter_routes(df)->list:
+    """
+    Filters and returns routes with average 'truck' values greater than 7.
+
+    Args:
+        df (pandas.DataFrame)
+
+    Returns:
+        list: List of route names with average 'truck' values greater than 7.
+    """
+    # Write your logic here
+
+    return list()
+
+
+def multiply_matrix(matrix)->pd.DataFrame:
+    """
+    Multiplies matrix values with custom conditions.
+
+    Args:
+        matrix (pandas.DataFrame)
+
+    Returns:
+        pandas.DataFrame: Modified matrix with values multiplied based on custom conditions.
+    """
+    # Write your logic here
+
+    return matrix
+
+
+def time_check(df)->pd.Series:
+    """
+    Use shared dataset-2 to verify the completeness of the data by checking whether the timestamps for each unique (`id`, `id_2`) pair cover a full 24-hour and 7 days period
+
+    Args:
+        df (pandas.DataFrame)
+
+    Returns:
+        pd.Series: return a boolean series
+    """
+    # Write your logic here
+
+    return pd.Series()
--- python_task_2.py
+++ python_task_2.py
@@ -1,172 +1,78 @@
-Question 1:
 import pandas as pd
 
-def calculate_distance_matrix(dataset_path):
-    # Load the CSV file into a DataFrame
-    df = pd.read_csv(dataset_path)
 
-    # Create a DataFrame with 'id' as index and columns
-    distance_matrix = pd.DataFrame(index=df['id'].unique(), columns=df['id'].unique())
+def calculate_distance_matrix(df)->pd.DataFrame():
+    """
+    Calculate a distance matrix based on the dataframe, df.
 
-    # Set diagonal values to 0
-    distance_matrix = distance_matrix.fillna(0)
+    Args:
+        df (pandas.DataFrame)
 
-    # Iterate over the rows of the DataFrame to calculate cumulative distances
-    for _, row in df.iterrows():
-        source_id = row['source']
-        dest_id = row['destination']
-        distance = row['distance']
-
-        # Update the cumulative distances for bidirectional routes
-        distance_matrix.at[source_id, dest_id] += distance
-        distance_matrix.at[dest_id, source_id] += distance
-
-    return distance_matrix
-
-# Replace 'dataset-3.csv' with the actual path to the dataset file
-result_dataframe = calculate_distance_matrix('dataset-3.csv')
-
-# Display the result DataFrame
-print(result_dataframe)
-
-Question 2:
-
-import pandas as pd
-
-def unroll_distance_matrix(input_dataframe):
-    # Get the column and index names from the input DataFrame
-    ids = input_dataframe.index
-
-    # Create an empty DataFrame to store unrolled distances
-    unrolled_distances = pd.DataFrame(columns=['id_start', 'id_end', 'distance'])
-
-    # Iterate through the combinations of id_start and id_end
-    for id_start in ids:
-        for id_end in ids:
-            # Skip combinations where id_start is equal to id_end
-            if id_start != id_end:
-                # Get the distance value from the input DataFrame
-                distance = input_dataframe.loc[id_start, id_end]
-
-                # Append the values to the unrolled_distances DataFrame
-                unrolled_distances = unrolled_distances.append({
-                    'id_start': id_start,
-                    'id_end': id_end,
-                    'distance': distance
-                }, ignore_index=True)
-
-    return unrolled_distances
-
-# Assuming result_dataframe is the DataFrame obtained from the calculate_distance_matrix function in Question 2
-# Replace it with the actual DataFrame variable
-result_dataframe = calculate_distance_matrix('dataset-3.csv')
-
-# Apply the function and display the result
-unrolled_dataframe = unroll_distance_matrix(result_dataframe)
-print(unrolled_dataframe)
-
-Question 3:
-import pandas as pd
-
-def find_ids_within_ten_percentage_threshold(input_dataframe, reference_value):
-    # Filter rows based on the reference value
-    reference_rows = input_dataframe[input_dataframe['id_start'] == reference_value]
-
-    # Calculate the average distance for the reference value
-    avg_distance = reference_rows['distance'].mean()
-
-    # Define the threshold as 10% of the average distance
-    threshold = 0.1 * avg_distance
-
-    # Filter rows where the distance is within the 10% threshold
-    selected_rows = input_dataframe[
-        (input_dataframe['id_start'] != reference_value) &  # Exclude the reference value itself
-        (input_dataframe['distance'] >= avg_distance - threshold) &
-        (input_dataframe['distance'] <= avg_distance + threshold)
-    ]
-
-    # Get the unique values from the 'id_start' column and sort them
-    result_list = sorted(selected_rows['id_start'].unique())
-
-    return result_list
-
-# Assuming unrolled_dataframe is the DataFrame obtained from the unroll_distance_matrix function in the previous step
-# Replace it with the actual DataFrame variable 
-unrolled_dataframe = unroll_distance_matrix(result_dataframe)
-
-# Example: Find IDs within 10% threshold for the reference value 123
-reference_value = 123
-result_list = find_ids_within_ten_percentage_threshold(unrolled_dataframe, reference_value)
-
-# Display the result list
-print(result_list)
-
-Question 4:
-import pandas as pd
-
-def calculate_toll_rate(input_dataframe):
-    df = input_dataframe.copy()
-
-    # Define rate coefficients for each vehicle type
-    rate_coefficients = {'moto': 0.8, 'car': 1.2, 'rv': 1.5, 'bus': 2.2, 'truck': 3.6}
-
-    # Calculate toll rates for each vehicle type
-    for vehicle_type, rate_coefficient in rate_coefficients.items():
-        df[vehicle_type] = df['distance'] * rate_coefficient
+    Returns:
+        pandas.DataFrame: Distance matrix
+    """
+    # Write your logic here
 
     return df
 
-# Assuming unrolled_dataframe is the DataFrame obtained from the unroll_distance_matrix function in Question 3
-# Replace it with the actual DataFrame variable
-unrolled_dataframe = unroll_distance_matrix(result_dataframe)
 
-# Apply the function and display the result
-toll_rate_dataframe = calculate_toll_rate(unrolled_dataframe)
-print(toll_rate_dataframe)
+def unroll_distance_matrix(df)->pd.DataFrame():
+    """
+    Unroll a distance matrix to a DataFrame in the style of the initial dataset.
 
-Question 5:
-import pandas as pd
-from datetime import datetime, timedelta, time
+    Args:
+        df (pandas.DataFrame)
 
-def calculate_time_based_toll_rates(input_dataframe):
-    df = input_dataframe.copy()
-
-    # Define time ranges and corresponding discount factors
-    time_ranges = [
-        ((0, 0, 0), (10, 0, 0), 0.8),
-        ((10, 0, 0), (18, 0, 0), 1.2),
-        ((18, 0, 0), (23, 59, 59), 0.8)
-    ]
-
-    # Define a constant discount factor for weekends
-    weekend_discount_factor = 0.7
-
-    # Create new columns for start_day, start_time, end_day, and end_time
-    df['start_day'] = df['startDay'].apply(lambda x: datetime.strptime(str(x), '%Y-%m-%d').strftime('%A'))
-    df['end_day'] = df['endDay'].apply(lambda x: datetime.strptime(str(x), '%Y-%m-%d').strftime('%A'))
-    df['start_time'] = df.apply(lambda row: time(row['startTime'] // 100, row['startTime'] % 100, 0), axis=1)
-    df['end_time'] = df.apply(lambda row: time(row['endTime'] // 100, row['endTime'] % 100, 0), axis=1)
-
-    # Calculate toll rates based on time intervals
-    for time_range in time_ranges:
-        start_time, end_time, discount_factor = time_range
-        mask = ((df['start_day'].isin(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']) &
-                 (df['start_time'] >= time(*start_time)) & (df['start_time'] < time(*end_time))) |
-                (df['start_day'].isin(['Saturday', 'Sunday'])))
-        df.loc[mask, ['moto', 'car', 'rv', 'bus', 'truck']] *= discount_factor
-
-    # Apply a constant discount factor for weekends
-    weekend_mask = df['start_day'].isin(['Saturday', 'Sunday'])
-    df.loc[weekend_mask, ['moto', 'car', 'rv', 'bus', 'truck']] *= weekend_discount_factor
+    Returns:
+        pandas.DataFrame: Unrolled DataFrame containing columns 'id_start', 'id_end', and 'distance'.
+    """
+    # Write your logic here
 
     return df
 
-# Assuming toll_rate_dataframe is the DataFrame obtained from the calculate_toll_rate function in Question 4
-# Replace it with the actual DataFrame variable
-toll_rate_dataframe = calculate_toll_rate(unrolled_dataframe)
 
-# Apply the function and display the result
-time_based_toll_rates_dataframe = calculate_time_based_toll_rates(toll_rate_dataframe)
-print(time_based_toll_rates_dataframe)
+def find_ids_within_ten_percentage_threshold(df, reference_id)->pd.DataFrame():
+    """
+    Find all IDs whose average distance lies within 10% of the average distance of the reference ID.
+
+    Args:
+        df (pandas.DataFrame)
+        reference_id (int)
+
+    Returns:
+        pandas.DataFrame: DataFrame with IDs whose average distance is within the specified percentage threshold
+                          of the reference ID's average distance.
+    """
+    # Write your logic here
+
+    return df
 
 
+def calculate_toll_rate(df)->pd.DataFrame():
+    """
+    Calculate toll rates for each vehicle type based on the unrolled DataFrame.
+
+    Args:
+        df (pandas.DataFrame)
+
+    Returns:
+        pandas.DataFrame
+    """
+    # Wrie your logic here
+
+    return df
+
+
+def calculate_time_based_toll_rates(df)->pd.DataFrame():
+    """
+    Calculate time-based toll rates for different time intervals within a day.
+
+    Args:
+        df (pandas.DataFrame)
+
+    Returns:
+        pandas.DataFrame
+    """
+    # Write your logic here
+
+    return df
